ac6d21e87e1b9680e73304842987a89cc46e4d88
Tania <tania.a.allen@gmail.com>
2016-06-29 15:57:06 +0000

    adding mongoose

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-var express = require('express');
-var app     = express();
+var express  = require('express');
+var mongoose = require('mongoose');
+var app      = express();
+
+var port = process.env.PORT || 3000;
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/herokutest';
+
+var Person = mongoose.model('Person', {
+    name: String
+});
 
 app.get('/', function(req,res) {
-    res.send('works!');
+    Person.create({name:'Tania'}, function() {
+       res.send('works!'); 
+    });
+});
+
+mongoose.connect(mongoDBURI);
+
+mongoose.connection.once('open', function() {
+    console.log('connected to mongod');   
 });
 
 
-
-var port = process.env.PORT || 3000;
 
 app.listen(port, process.env.IP, function() {
     console.log('listening on port ' + process.env.PORT);
